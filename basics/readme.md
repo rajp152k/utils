b17682ab92298dca76311a4af45a60e405807810
rajp152k <rajp152k@gmail.com>
2024-01-21 09:13:58 +0530

    timer restructured

---
+++ basics/readme.md
@@ -0,0 +1,18 @@
+# A basic timer
+
+A basic timer.
+
+## Usage 
+
+Python 3.10+ as I'm using "|" instead of Unions when type hinting.
+`aplay` to ring the alarm.
+
+``` bash
+chmod +x timer.py
+alias timer "python3 /<path-to-parent-dir>/timer.py"
+
+timer   -H <hours, default=0>   \
+        -M <mins, default=0>    \
+        -S <seconds, default=0> \
+        -R <ring, default=<some file on my system, do change this>>
+```
--- basics/timer.py
+++ basics/timer.py
@@ -1,5 +1,3 @@
-#!/home/rp152k/miniforge3/bin/python
-
 from pprint import pprint
 import subprocess
 from pathlib import Path
@@ -8,6 +6,7 @@ import time
 import asyncio
 
 aper = argparse.ArgumentParser("Basic Alarm")
+NOTIF = Path("/usr/share/sounds/sound-icons/canary-long.wav")
 
 aper.add_argument("-H", "--hours", type=int,
                   default=0, help="time in hours")
@@ -15,36 +14,56 @@ aper.add_argument("-M", "--minutes", type=int,
                   default=0, help="time in minutes")
 aper.add_argument("-S", "--seconds", type=int,
                   default=0, help="time in seconds")
+aper.add_argument("-R", "--ring", type=str,
+                  default=NOTIF, help="path to notification sound")
 
-NOTIF = Path("/usr/share/sounds/sound-icons/canary-long.wav")
+
 RED_BACKGROUND = '\033[41m'
 RESET_FORMATTING = '\033[0m'
+DELETE_LINE = '\x1b[2K'
 
 
 def notify_text(message: str | None = None):
     message = "notification triggered" if message is None else message
     print(RED_BACKGROUND)
     pprint(message)
-    print(RESET_FORMATTING)
 
 
-def notify_alarm(interval: int = 1):
+def notify_alarm(interval: int = 1, ring=NOTIF):
     try:
         print("Hit C-c to stop alarm")
         while True:
-            subprocess.run(['/usr/bin/aplay','-q', str(NOTIF)])
+            print("RINGING", end="\r")
+            subprocess.run(['/usr/bin/aplay', '-q', str(ring)])
             time.sleep(interval)
+            print(DELETE_LINE)
     except KeyboardInterrupt:
+        print(RESET_FORMATTING)
         print("alarm stopped")
 
 
 async def sleep(time):
     await asyncio.sleep(time)
 
+
+def args_handler(args: argparse.Namespace, wait_seconds: int = 0):
+    if not Path(args.ring).exists():
+        print("notification sound not found")
+        # print help message of args
+        aper.print_help()
+        exit(1)
+
+    if wait_seconds <= 0:
+        print("time must be positive")
+        # print help message of args
+        aper.print_help()
+        exit(1)
+
+
 if __name__ == '__main__':
     args = aper.parse_args()
     wait_seconds = args.hours * 3600 + args.minutes * 60 + args.seconds
-
+    args_handler(args, wait_seconds)
     print(f"alarm init  {args.hours} H, {args.minutes} M,  {args.seconds} S")
     asyncio.run(sleep(wait_seconds))
     notify_text()
